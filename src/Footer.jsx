e3121805df0e4f4e9c881358ad0b74bb708c8d6a
Julia-SP <holajuliasubiza@gmail.com>
2025-07-17 08:57:43 +0200

    html y css footer terminado

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ import './styles_footer.css'
 function Footer() {
   return (
     <div className='container'>
-      <p>logo </p>
+ <img src="./public/LOGO_60_blanco.svg" alt="logo"></img>
     <div className="content">
    <h3>...y si lo terminas?</h3>
    <h4>¡Escríbeme y recibirás una sorpresa!</h4>
