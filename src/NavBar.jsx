86a50a5b5d9b9f17b690f6196750a868dd9059f8
Julia-SP <holajuliasubiza@gmail.com>
2025-08-25 20:01:28 +0200

    creo que navbar movil bien

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,14 +1,21 @@
 import '/public/styles_navBar.css'
 
+import { useState } from "react";
+
 export default function NavBar() {
+  const [open, setOpen] = useState(false);
+
   return (
     <nav className="navbar">
-      <ul className='contenido'>
-        <li id="logo">
-          <a href="#hero">
-            <img src="./LOGO_60_morado.svg" alt="logo" />
-          </a>
-        </li>
+      <a href="#hero" id="logo">
+        <img src="./LOGO_60_morado.svg" alt="logo" />
+      </a>
+
+      <button className="hamburger" onClick={() => setOpen(!open)}>
+        {open ? "✕" : "☰"}
+      </button>
+
+      <ul className={`contenido ${open ? "open" : ""}`}>
         <li className="boton-nav"><a href="#container-sec1">WTH?</a></li>
         <li className="boton-nav"><a href="#container-sec2">Retos</a></li>
         <li className="boton-nav"><a href="#container-footer">Contacto</a></li>
